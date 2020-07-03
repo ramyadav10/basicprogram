554a08aae3dc2a11babb02c8056eaa45b80a0079
Ram Yadav <ramyadav.now@gmail.com>
2020-07-03 14:39:24 +0530

    Update caseEmpwage.sh

--- day10-githubUC/caseEmpwage.sh
+++ day10-githubUC/caseEmpwage.sh
@@ -1,13 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash
 isFullTime=1;
 isPartTime=2;
-empRate=100;
+empRateHr=20;
+totalSalary=0;
+numWorkingDays=20;
+for (( i=1; i<=$numWorkingDays; i++ ))
+do
 randomCheck=$(( RANDOM % 3))
 case "$randomCheck" in
       $isFullTime) workHrs=8 ;;
       $isPartTime) workHrs=4 ;;
       *) workHrs=0 ;;
 esac
-salary=$(( $empRate * $workHrs ))
-echo $salary
+salary=$(( $empRateHr * $workHrs ))
+totalSalary=$(( $totalSalary+$salary ))
+done
+echo $totalSalary
+
 
